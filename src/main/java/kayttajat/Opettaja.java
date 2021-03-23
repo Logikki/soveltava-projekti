a85237e2b1186b93283a91d2c8737b37e350aba9
Aleksi K <ajkouh@utu.fi>
2021-03-23 15:37:55 +0200

    opettaja -luokka

--- src/main/java/kayttajat/Opettaja.java
+++ src/main/java/kayttajat/Opettaja.java
@@ -1,3 +1,36 @@
+package kayttajat;
 class Opettaja{
-    
+    private String nimi;
+    private String salasana;
+    private String sposti;
+
+    public Opettaja(String nimi, String salasana, String sposti) {
+        this.nimi = nimi;
+        this.salasana = salasana;
+        this.sposti = sposti;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public String getSalasana() {
+        return salasana;
+    }
+
+    public void setSalasana(String salasana) {
+        this.salasana = salasana;
+    }
+
+    public String getSposti() {
+        return sposti;
+    }
+
+    public void setSposti(String sposti) {
+        this.sposti = sposti;
+    }
 }
