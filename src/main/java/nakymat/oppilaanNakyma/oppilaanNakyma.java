5e835cba74671903d67d574a1418337666681b69
Roni Koskinen <roni.p.koskinen@utu.fi>
2021-04-05 16:52:22 +0300

    viimeisin versio

--- src/main/java/nakymat/oppilaanNakyma/oppilaanNakyma.java
+++ src/main/java/nakymat/oppilaanNakyma/oppilaanNakyma.java
@@ -1,12 +1,19 @@
 package nakymat.oppilaanNakyma;
 import kayttajat.opiskelija.Opiskelija;
+import javax.swing.*;
+import java.awt.event.*;    
 
 public class oppilaanNakyma {
     protected Opiskelija kayttaja;
-
+	JFrame ruutu;
+	JLabel tervetuloa;
+	
 	public oppilaanNakyma(Opiskelija kayttaja) {
 		this.kayttaja = kayttaja;
         System.out.println("Opiskelijanakyma luotu");
+		ruutu = new JFrame();
+		ruutu.setSize(400,400); ruutu.setVisible(true); ruutu.setLayout(null);
+
 	}
     
 }
