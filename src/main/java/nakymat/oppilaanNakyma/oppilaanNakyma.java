8c9298d3ea02fe4713751269d5f080608a9283e7
Roni Koskinen <roni.p.koskinen@utu.fi>
2021-04-05 18:29:54 +0300

    tehty opiskelijanäkymää

--- src/main/java/nakymat/oppilaanNakyma/oppilaanNakyma.java
+++ src/main/java/nakymat/oppilaanNakyma/oppilaanNakyma.java
@@ -1,19 +1,33 @@
 package nakymat.oppilaanNakyma;
 import kayttajat.opiskelija.Opiskelija;
 import javax.swing.*;
-import java.awt.event.*;    
+import java.awt.event.*;
+import java.util.HashMap;    
 
 public class oppilaanNakyma {
     protected Opiskelija kayttaja;
 	JFrame ruutu;
-	JLabel tervetuloa;
-	
+	JLabel tervetuloa,header;
 	public oppilaanNakyma(Opiskelija kayttaja) {
 		this.kayttaja = kayttaja;
-        System.out.println("Opiskelijanakyma luotu");
+		DefaultListModel<String> suoritukset = new DefaultListModel<>();  
+		HashMap<String,String> kurssit = kayttaja.getKurssiSuoritukset();
 		ruutu = new JFrame();
-		ruutu.setSize(400,400); ruutu.setVisible(true); ruutu.setLayout(null);
+		ruutu.setSize(300,300); ruutu.setVisible(true); ruutu.setLayout(null);
+		tervetuloa = new JLabel("Tervetuloa " + kayttaja.getNimi());
+		ruutu.add(tervetuloa);
+		tervetuloa.setBounds(80, -10, 150, 50);
+		header = new JLabel("Arvosana     Kurssin nimi");
+		header.setBounds(60, 10, 250, 50);
+		 ruutu.add(header);
+		for (String kurssi : kurssit.keySet()) {
+			suoritukset.addElement(kurssit.get(kurssi) + "     " + kurssi );
+		}
+		JList<String> list = new JList<>(suoritukset);
+		list.setBounds(60,50, 200,200);  
+		ruutu.add(list);    
+		
 
+}
 	}
     
-}
