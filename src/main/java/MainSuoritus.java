22988c1e7c9d643acbfd2d7932125168beed074c
Roni Koskinen <roni.p.koskinen@utu.fi>
2021-04-08 13:29:01 +0300

    mainsuoritus

--- src/main/java/MainSuoritus.java
+++ src/main/java/MainSuoritus.java
@@ -1,14 +1,7 @@
-import kayttajat.opiskelija.Opiskelija;
 import nakymat.aloitysnaytto.Aloitusnaytto;
-import nakymat.oppilaanNakyma.oppilaanNakyma;
 
 public class MainSuoritus {
     public static void main(String[] args) {
-        Opiskelija Matti = new Opiskelija("matti", "salasana", "123", "mattimeikalainne@moi.fi");
-        Matti.lisaaKurssiSuoritus("historia", "5");
-        Matti.lisaaKurssiSuoritus("ruotsi", "5");
-        Matti.lisaaKurssiSuoritus("kemia", "5");
-        Matti.lisaaKurssiSuoritus("fysiikka", "5");
-        new oppilaanNakyma(Matti);
+        new Aloitusnaytto();
         }
 }
